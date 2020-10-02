8c36ec37143b53b74cdbeb7b7f01b1d1fe34d641
Justice Okpala <justicecharles497@gmail.com>
2020-10-02 01:38:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn4");
+let btn6 = document.getElementById("btn4");
+let btn7 = document.getElementById("btn4");
+let btn8 = document.getElementById("btn4");
+let btn9 = document.getElementById("btn4");
+let btn10 = document.getElementById("btn4");
+let btn411= document.getElementById("btn4");
+let btn412= document.getElementById("btn4");
+let btn413 = document.getElementById("btn4");
+let btn414 = document.getElementById("btn4");
+let btn415 = document.getElementById("btn4");
+let btn416 = document.getElementById("btn4");
+let btn417 = document.getElementById("btn4");
+let btn418 = document.getElementById("btn4");
+let btn419 = document.getElementById("btn4");
+let btn420 = document.getElementById("btn4");
+let btn421 = document.getElementById("btn4");
+let btn422 = document.getElementById("btn4");
+let btn423 = document.getElementById("btn4");
+let btn424 = document.getElementById("btn4");
+let btn425 = document.getElementById("btn4");
+
+
+
+function openNewWindow(e,path){
+    e.preventDefault();
+    window.open(path,'_blank',"width=500px,height=500px");
+}
+
